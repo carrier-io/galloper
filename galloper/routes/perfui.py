0329a4f5182d7a89cb874dcd63f3a4c16126f068
arozumenko <artyom.rozumenko@gmail.com>
2019-10-19 09:27:08 +0300

    WIP: added perfui endpoint

--- galloper/routes/perfui.py
+++ galloper/routes/perfui.py
@@ -12,9 +12,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from flask import Blueprint, request, render_template, flash, current_app, redirect, url_for
+from flask import Blueprint, request
 from galloper.processors.perfui import prepareReport
+from galloper.constants import check_ui_performance
 import tempfile
+from time import sleep
 from json import loads
 from os.path import join
 from shutil import rmtree
@@ -44,3 +46,37 @@ def analyze_sync():
         report = prepareReport(video_path, loads(request.form.get('data')), videofolder, True)
         rmtree(videofolder)
         return report.get_report()
+
+
+@bp.route('/perfui/test', methods=["POST"])
+def run_test():
+    url = request.form.get('url')
+    remote_driver_address = request.form.get("remote_address", "127.0.0.1:4444")
+    listener_address = request.form.get("perfui_listener", "127.0.0.1:9999")
+    from selenium import webdriver
+    from requests import get
+    from time import time
+    start_time = time()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--window-size=1360,1020')
+    driver = webdriver.Remote(
+        command_executor=f'http://{remote_driver_address}/wd/hub',
+        desired_capabilities=chrome_options.to_capabilities())
+    get(f'http://{listener_address}/record/start')
+    current_time = time() - start_time
+    driver.get(url)
+    while not driver.execute_script('return document.readyState === "complete" && performance.timing.loadEventEnd > 0'):
+        sleep(0.1)
+    results = driver.execute_script(check_ui_performance)
+    video_results = get(f'http://{listener_address}/record/stop').content
+    results['info']['testStart'] = int(current_time)
+    driver.quit()
+    videofolder = tempfile.mkdtemp()
+    video_path = join(videofolder, "Video.mp4")
+    with open(video_path, 'w+b') as f:
+        f.write(video_results)
+    report = prepareReport(video_path, results, videofolder, True)
+    #rmtree(videofolder)
+    return report.get_report()
+
+
