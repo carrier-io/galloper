24b14010155d811467a9f642792285798eab6946
hunkom <46744804+hunkom@users.noreply.github.com>
2019-11-20 16:36:52 +0200

    Update post-processing lambda (#8)

--- lambdas/post_processing/lambda_function.py
+++ lambdas/post_processing/lambda_function.py
@@ -4,9 +4,9 @@ from perfreporter.post_processor import PostProcessor
 
 def lambda_handler(event, context):
     try:
-        args, comparison_data, aggregated_errors, errors, config_file = parse_event(event)
-        post_processor = PostProcessor(args, aggregated_errors, errors, comparison_data, config_file)
-        post_processor.distributed_mode_post_processing()
+        args, aggregated_errors, config_file = parse_event(event)
+        post_processor = PostProcessor(args, aggregated_errors, config_file)
+        post_processor.post_processing()
 
     except Exception as e:
         return {
@@ -25,9 +25,7 @@ def parse_event(_event):
     event = _event if not _event.get('body') else json.loads(_event['body'])
 
     args = json.loads(event.get('arguments'))
-    test_results = json.loads(event.get('test_results'))
     aggregated_errors = json.loads(event.get('aggregated_errors'))
-    errors = json.loads(event.get('errors'))
     config_file = json.loads(event.get('config_file'))
 
-    return args, test_results, aggregated_errors, errors, config_file
+    return args, aggregated_errors, config_file
