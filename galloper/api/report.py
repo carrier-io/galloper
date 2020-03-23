2239d5cb858a903fd49f8bac2571cbc7727c3c04
Artem Rozumenko <artyom.rozumenko@gmail.com>
2020-03-23 13:50:44 +0200

    added quick fix for security reports

--- galloper/api/report.py
+++ galloper/api/report.py
@@ -391,11 +391,12 @@ class FindingsAPI(Resource):
             finding["details"] = hash_id.id
             finding['project_id'] = project_id
             entrypoints = ""
-            for each in finding.get("endpoints"):
-                if isinstance(each, list):
-                    entrypoints += "<br />".join(each)
-                else:
-                    entrypoints += f"<br />{each}"
+            if finding.get("endpoints"):
+                for each in finding.get("endpoints"):
+                    if isinstance(each, list):
+                        entrypoints += "<br />".join(each)
+                    else:
+                        entrypoints += f"<br />{each}"
             finding["endpoints"] = entrypoints
             if not (finding["false_positive"] == 1 or finding["excluded_finding"] == 1):
                 # TODO: add validation that finding is a part of project, applicaiton. etc.
