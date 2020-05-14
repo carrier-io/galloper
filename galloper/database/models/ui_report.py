3bebe9a75fea87c68f804e8cb878a854e74f364d
Artem Rozumenko <artyom.rozumenko@gmail.com>
2020-05-14 19:42:57 +0300

    added thresholds for ui performance

--- galloper/database/models/ui_report.py
+++ galloper/database/models/ui_report.py
@@ -7,14 +7,14 @@ class UIReport(AbstractBaseMixin, Base):
     __tablename__ = "ui_report"
 
     id = Column(Integer, primary_key=True)
-    test_name = Column(String(128), unique=False)
+    name = Column(String(128), unique=False)
     project_id = Column(Integer, unique=False, nullable=False)
     start_time = Column(String(128), unique=False)
     stop_time = Column(String(128), unique=False)
     duration = Column(Integer, unique=False, nullable=True)
     is_active = Column(Boolean, unique=False)
     browser = Column(String(128), unique=False)
-    env = Column(String(128), unique=False, nullable=True)
+    environment = Column(String(128), unique=False, nullable=True)
     base_url = Column(String(128), unique=False)
     visited_pages = Column(Integer, unique=False, nullable=True)
     thresholds_total = Column(Integer, unique=False, nullable=True, default=0)
