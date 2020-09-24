4c9bc0b3d1daa0859ef62dbc2373ccdf5c103dea
mykhailo_hunko <mykhailo.hunko@epam.com>
2020-09-24 19:41:59 +0300

    Added git to the backend performance tests

--- migration/migrate.sql
+++ migration/migrate.sql
@@ -6,3 +6,6 @@ ALTER TABLE ui_report ADD COLUMN uid VARCHAR(128) NOT NULL;
 ALTER TABLE ui_result DROP COLUMN report_id;
 ALTER TABLE ui_result ADD COLUMN report_uid VARCHAR(128);
 ALTER TABLE api_report ADD COLUMN status VARCHAR(128);
+
+#
+ALTER TABLE performance_tests ADD COLUMN git JSON;
